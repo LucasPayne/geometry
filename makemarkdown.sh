7d07c0c78ffcba0eda7b5b33f9b26ddef722970d
LucasPayne <lucaspayyne@gmail.com>
2019-10-23 14:50:12 +1300

    readme

--- makemarkdown.sh
+++ makemarkdown.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-for f in Python/images/*; do
-    convert $f -trim $f
+rm Python/images/created/*
+
+find Python/images/ -type f -name "*.png" -o -name "*.gif" | while IFS= read -r f; do
+    created_filename="Python/images/created/$(basename "$f")"
+    convert "$f" -trim -colorspace Gray "$created_filename"
 done
 
 printf "Messing around with computational geometry.\n\n" > README.md
 
-for f in Python/images/*; do
-    dimensions=$(identify $f | awk '{print $3; exit}')
+find Python/images/created/ -type f -name "*.png" -o -name "*.gif" | while IFS= read -r f; do
+    dimensions=$(identify "$f" | awk '{print $3; exit}')
     width=$(echo $dimensions | awk -F "x" '{print $1}')
     height=$(echo $dimensions | awk -F "x" '{print $2}')
-
+    echo "woo "$f""
     normalizing_height=300
+
+    echo "$width"
+    echo "$height"
+    echo "$normalizing_height"
 
     # Normalize to a specific height
     mul=$(echo "($normalizing_height * 10000) / $height" | bc)
 
-    height=$normalizing_height
-    width=$(echo "($width * $mul) / 10000" | bc)
+    # height=$normalizing_height
+    # width=$(echo "($width * $mul) / 10000" | bc)
 
     printf "<img src=\"$f\" alt=\"$f\" width=\"$width\" height=\"$height\"> " >> README.md
 done
